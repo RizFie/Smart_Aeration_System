f3211ddddd27e63ef8f28f42b75b1a248da04b36
Hariz Rafie <harizrafie86@gmail.com>
2026-06-09 23:53:44 +0800

    Add DS18B20 support and ESP32-S3 config
    Add VSCode Arduino port config and switch PlatformIO environment to ESP32-S3 dev board. platformio.ini: rename env to esp32s3dev, set board to esp32-s3-devkitc-1, and add OneWire and DallasTemperature libraries. Smaraetor.ino: integrate OneWire/DallasTemperature (ONE_WIRE_BUS=4), initialize sensors, request temperatures and display the measured Celsius value on the SSD1306 OLED (with 2s delays). Also keep existing OLED/Wire initialization. These changes enable reading a DS18B20 temperature sensor and showing its value on the display.

--- src/Smaraetor.ino
+++ src/Smaraetor.ino
@@ -2,26 +2,42 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
+#define ONE_WIRE_BUS 4
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 
 void setup() {
   Serial.begin(115200);
+  delay(2000);
+  sensors.begin();
   Wire.begin(8, 9); // SDA, SCL
-
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-  display.println("Hello, Hariz!");
-  display.setCursor(0, 16);
-  display.println("ESP32-S3 works!");
-  display.display();
+
 }
 
-void loop() {}
+void loop() {
+  sensors.requestTemperatures();
+
+  float tempC = sensors.getTempCByIndex(0);
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("Temperature: ");
+  display.setCursor(0, 16);
+  display.print(tempC, 2);
+  display.println(" Degree Celsius");
+  display.display();
+
+  delay(2000);
+}
