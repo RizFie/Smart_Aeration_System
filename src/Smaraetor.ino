17e662622e7a5456d65f9e6b389d4f3e0817f96b
Hariz Rafie <harizrafie86@gmail.com>
2026-06-09 23:32:35 +0800

    Initial commit

---
+++ src/Smaraetor.ino
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(8, 9); // SDA, SCL
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("Hello, Hariz!");
+  display.setCursor(0, 16);
+  display.println("ESP32-S3 works!");
+  display.display();
+}
+
+void loop() {}
